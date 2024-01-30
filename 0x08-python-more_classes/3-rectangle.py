a0136eb306caf2443c8a47d5606f37a8f488aa37
Nigel Waswa <waswanigel@gmail.com>
2024-01-30 02:02:47 +0000

    task 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,12 +55,12 @@ class Rectangle:
 
         Represents the rectangle with the # character.
         """
-    if self.__width == 0 or self.__height == 0:
-        return("")
+        if self.__width == 0 or self.__height == 0:
+            return("")
 
-    rect = []
-    for i in range(self.__height):
-        [rect.append('#') for j in range(self.__width)]
-        if i != self.__height - 1:
-            rect.append("\n")
-    return ("".join(rect))
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
