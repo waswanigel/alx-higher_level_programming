54bcd922cd5726cd2656e9004425bd867f44057a
Nigel Waswa <waswanigel@gmail.com>
2024-04-26 20:23:03 +0000

    commit 2

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""A script that:
-    - takes in a URL,
-    - sends a request to the URL and displays the value
-    - of the X-Request-Id variable found in the header ofthe response.
 """
-import sys
-import urllib.request
+Python script that takes in a URL and
+an email address, sends a POST request
+"""
+from sys import argv
+import requests
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.post(argv[1], data={'email': argv[2]})
+    print(r.text)
