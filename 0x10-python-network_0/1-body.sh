f982964a52c65bed6916c465191ca9209e33d45d
Nigel Waswa <waswanigel@gmail.com>
2024-04-28 09:54:58 +0000

    corrected 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# Bash script that takes in a URL, sends a GET request to the URL,
+# and displays the body of the response
+if [ "$(curl -sLw "%{http_code}" "$1" -o /dev/null)" -eq "200" ]; then curl -sL "$1"; fi # curl -sIL "$1" | grep "HTTP/1.0 200 OK" > /dev/null && curl -sL "$1"
